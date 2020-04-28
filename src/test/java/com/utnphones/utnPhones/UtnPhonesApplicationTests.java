a57d0ea130b94594206945163141dc05460bbab2
Matias Nicoletti <matiasnicolettmdp@gmail.com>
2020-04-28 14:27:59 -0300

    first

---
+++ src/test/java/com/utnphones/utnPhones/UtnPhonesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utnphones.utnPhones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UtnPhonesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
