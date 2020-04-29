26fa0a8210f1d84b353e5e88bdc9cfe45f981b85
Federico Anastasi <fede_3775@hotmail.com>
2020-04-29 20:44:34 -0300

    Schema and pom modified

--- schema/utn_phones.sql
+++ schema/utn_phones.sql
@@ -1,7 +1,5 @@
-drop database utn_phones;
 CREATE DATABASE utn_phones;
 USE utn_phones;
-
 
 CREATE TABLE Provinces(
     id_province INT AUTO_INCREMENT,
@@ -13,26 +11,48 @@ CREATE TABLE Cities(
     id_city INT AUTO_INCREMENT,
     id_province INT NOT NULL,
     city_name VARCHAR(50) NOT NULL,
+    prefix varchar(5) not null,
     CONSTRAINT PK_CITIES PRIMARY KEY (id_city),
-    CONSTRAINT FK_CITIES_PROVINCES FOREIGN KEY (id_province) REFERENCES Provinces(id_province)
+    CONSTRAINT FK_CITIES_PROVINCES FOREIGN KEY (id_province) REFERENCES Provinces(id_province),
+    CONSTRAINT UNQ_PREFIX unique(prefix)
 );
 
-CREATE TABLE Clients(
-    id_client INT AUTO_INCREMENT,
+CREATE TABLE Persons(
+    id_person INT AUTO_INCREMENT,
     id_city INT NOT NULL,
     firstname VARCHAR(50) NOT NULL,
     surname VARCHAR(50) NOT NULL, 
     DNI VARCHAR(9) NOT  NULL UNIQUE,
-    CONSTRAINT PK_CLIENTS PRIMARY KEY (id_client),
-    CONSTRAINT FK_CLIENTS_CITIES FOREIGN KEY (id_city) REFERENCES Cities(id_city)
+    CONSTRAINT PK_PERSONS PRIMARY KEY (id_person),
+    CONSTRAINT FK_PERSONS_CITIES FOREIGN KEY (id_city) REFERENCES Cities(id_city)
 );
 
-insert into Provinces(province_name) values ("Buenos Aires"), ("Mendoza");
-select * from provinces;
-insert into cities(id_province, city_name) values(1, "Mar del Plata"), (2, "San Rafael de Mendoza"), (1, "La Plata");
-select * from cities;
-insert into clients(id_city, firstname, surname, DNI) values (1, "John", "Doe", "45655147"), (2, "Pedro", "Lopez", "35645177");
-select * from clients;
+CREATE TABLE Clients(
+    id_client INT AUTO_INCREMENT,
+    id_person int not null,
+    username varchar(50) not null,
+    password varchar(50) not null,
+	constraint PK_CLIENTS primary key(id_client),
+    constraint FK_CLIENTS_PERSONS foreign key(id_person) references Persons(id_person)
+);
+
+CREATE TABLE Employees(
+	id_employee INT AUTO_INCREMENT,
+    id_person int not null,
+    username varchar(50) not null,
+    password varchar(50) not null,
+    constraint PK_EMPLOYEES primary key(id_employee),
+    constraint FK_EMPLOYEES_PERSONS foreign key(id_person) references Persons(id_person)
+);
+
+CREATE TABLE Fares(
+    id_city_from INT NOT NULL,
+    id_city_to INT NOT NULL,
+    price FLOAT NOT NULL,
+    CONSTRAINT PK_FARES PRIMARY KEY (id_city_from,id_city_to),
+    CONSTRAINT FK_CITY_FROM_CITIES FOREIGN KEY(id_city_from) REFERENCES Cities(id_city),
+    CONSTRAINT FK_CITY_TO_CITIES FOREIGN KEY(id_city_to) REFERENCES Cities(id_city)
+);
 
 CREATE TABLE Line_types(
     id_line_type INT AUTO_INCREMENT,
@@ -40,30 +60,17 @@ CREATE TABLE Line_types(
     CONSTRAINT PK_LINE_TYPES PRIMARY KEY (id_line_type)
 );
 
-CREATE TABLE Prefixes(
-    id_prefix INT AUTO_INCREMENT,
-    prefix VARCHAR(5) NOT NULL,
-    CONSTRAINT PK_PREFIXES PRIMARY KEY (id_prefix)
-);
-CREATE TABLE Fares(
-    id_fare INT AUTO_INCREMENT,
-    id_city_from INT NOT NULL,
-    id_city_to INT NOT NULL,
-    price FLOAT NOT NULL,
-    CONSTRAINT PK_FARES PRIMARY KEY (id_fare)
-);
 CREATE TABLE Phone_lines(
     id_phone_line INT AUTO_INCREMENT,
     id_line_type INT NOT NULL,
     id_client INT NOT NULL,
-    id_prefix INT NOT NULL,
     line_number VARCHAR(15) NOT NULL,
-    line_status INT(1) NOT NULL, -- -1 suspendida, 0 cancelada, 1 activada
+	line_status ENUM('active', 'canceled', 'suspended') NOT NULL,
     CONSTRAINT PK_PHONE_LINES PRIMARY KEY (id_phone_line),
     CONSTRAINT FK_LINES_LINE_TYPES FOREIGN KEY (id_line_type) REFERENCES Line_types(id_line_type),
-    CONSTRAINT FK_LINES_CLIENTS FOREIGN KEY (id_client) REFERENCES Clients(id_client),
-    CONSTRAINT FK_LINES_PREFIX FOREIGN KEY (id_prefix) REFERENCES Prefixes(id_prefix)
+    CONSTRAINT FK_LINES_CLIENTS FOREIGN KEY (id_client) REFERENCES Clients(id_client)
 );
+
 CREATE TABLE Invoices(
     id_invoice INT AUTO_INCREMENT,
     id_line INT NOT NULL,
@@ -80,11 +87,12 @@ CREATE TABLE Calls(
     id_call INT AUTO_INCREMENT,
     id_phone_line_from INT NOT NULL,
     id_phone_line_to INT NOT NULL,
-    id_fare INT NOT NULL,
+    id_invoice int not null,
+    fare float NOT NULL,
     duration INT NOT NULL,
+    total_price float not null,
     CONSTRAINT PK_CALLS PRIMARY KEY (id_call),
-    CONSTRAINT FK_CALLS_FARE FOREIGN KEY (id_fare) REFERENCES Fares(id_fare),
     CONSTRAINT FK_CALLS_PHONE_LINE_FROM FOREIGN KEY (id_phone_line_from) REFERENCES Phone_lines(id_phone_line),
-    CONSTRAINT FK_CALLS_PHONE_LINE_TO FOREIGN KEY (id_phone_line_to) REFERENCES Phone_lines(id_phone_line)
+    CONSTRAINT FK_CALLS_PHONE_LINE_TO FOREIGN KEY (id_phone_line_to) REFERENCES Phone_lines(id_phone_line),
+    CONSTRAINT FK_CALLS_INVOICES FOREIGN KEY (id_invoice) REFERENCES Invoices(id_invoice)
 );
-
