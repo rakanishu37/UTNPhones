2315fcd1c423a84f7c2accd876d68507d8611650
Matias Nicoletti <matiasnicolettmdp@gmail.com>
2020-06-21 18:54:17 -0300

    client service test progress

--- src/test/java/com/utnphones/utnPhones/service/ClientServiceTest.java
+++ src/test/java/com/utnphones/utnPhones/service/ClientServiceTest.java
@@ -1,6 +1,12 @@
 package com.utnphones.utnPhones.service;
 
+import com.utnphones.utnPhones.domain.City;
 import com.utnphones.utnPhones.domain.Client;
+import com.utnphones.utnPhones.domain.LineStatus;
+import com.utnphones.utnPhones.domain.LineType;
+import com.utnphones.utnPhones.domain.PhoneLine;
+import com.utnphones.utnPhones.domain.Province;
+import com.utnphones.utnPhones.dto.PhoneLineDTO;
 import com.utnphones.utnPhones.exceptions.ClientNotFoundException;
 import com.utnphones.utnPhones.repository.ClientRepository;
 import com.utnphones.utnPhones.repository.PhoneLineRepository;
@@ -13,6 +19,7 @@ import org.junit.Test;
 import org.mockito.Mock;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
@@ -44,5 +51,30 @@ public class ClientServiceTest {
 
         Assert.assertEquals(list.size(), listTest.size());
 
+    }
+
+    @Test
+    public void testGetId() throws ClientNotFoundException {
+        Client client = TestUtils.getClients().get(0);
+        when(this.clientRepository.findById(client.getId())).thenReturn(java.util.Optional.of(client));
+
+        Client clientTest = this.clientService.getById(client.getId());
+
+        Assert.assertEquals(client, clientTest);
+    }
+
+    @Test(expected = ClientNotFoundException.class)
+    public void testGetIdNotFound() throws ClientNotFoundException {
+
+        when(this.clientRepository.findById(1)).thenReturn(Optional.ofNullable(null));
+
+        this.clientService.getById(1);
+
+    }
+
+    @Test
+    public void testDeleteClient(){
+        Client client = TestUtils.getClients().get(0);
+        when(this.clientRepository.deleteClient(client.getId())).thenReturn()
     }
 }
